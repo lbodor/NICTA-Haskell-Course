44b020d65bd75a2ee2fb6c367c7a85b146c5d1d3
Lazar Bodor <lazar.bodor@ga.gov.au>
2015-06-11 21:01:33 +1000

    Solve Apply.

--- src/Course/Apply.hs
+++ src/Course/Apply.hs
@@ -35,8 +35,7 @@ instance Apply Id where
     Id (a -> b)
     -> Id a
     -> Id b
-  (<*>) =
-    error "todo: Course.Apply (<*>)#instance Id"
+  (Id f) <*> (Id a) = Id $ f a
 
 -- | Implement @Apply@ instance for @List@.
 --
@@ -47,8 +46,9 @@ instance Apply List where
     List (a -> b)
     -> List a
     -> List b
-  (<*>) =
-    error "todo: Course.Apply (<*>)#instance List"
+  fs <*> as = case fs of
+    Nil   -> Nil
+    f:.fs' -> map f as ++ (fs' <*> as)
 
 -- | Implement @Apply@ instance for @Optional@.
 --
@@ -65,8 +65,9 @@ instance Apply Optional where
     Optional (a -> b)
     -> Optional a
     -> Optional b
-  (<*>) =
-    error "todo: Course.Apply (<*>)#instance Optional"
+  Empty    <*> _ = Empty
+  (Full f) <*> a = f <$> a
+  
 
 -- | Implement @Apply@ instance for reader.
 --
@@ -86,11 +87,10 @@ instance Apply Optional where
 -- 15
 instance Apply ((->) t) where
   (<*>) ::
-    ((->) t (a -> b))
-    -> ((->) t a)
-    -> ((->) t b)
-  (<*>) =
-    error "todo: Course.Apply (<*>)#instance ((->) t)"
+    (t -> (a -> b))
+    -> (t -> a)
+    -> (t -> b)
+  f <*> g = \t -> f t $ g t
 
 -- | Apply a binary function in the environment.
 --
@@ -117,8 +117,7 @@ lift2 ::
   -> f a
   -> f b
   -> f c
-lift2 =
-  error "todo: Course.Apply#lift2"
+lift2 f a b = f <$> a <*> b
 
 -- | Apply a ternary function in the environment.
 --
@@ -149,8 +148,7 @@ lift3 ::
   -> f b
   -> f c
   -> f d
-lift3 =
-  error "todo: Course.Apply#lift2"
+lift3 f a b c = lift2 f a b <*> c
 
 -- | Apply a quaternary function in the environment.
 --
@@ -182,8 +180,7 @@ lift4 ::
   -> f c
   -> f d
   -> f e
-lift4 =
-  error "todo: Course.Apply#lift4"
+lift4 f a b c d = lift3 f a b c <*> d
 
 -- | Sequence, discarding the value of the first argument.
 -- Pronounced, right apply.
@@ -208,8 +205,7 @@ lift4 =
   f a
   -> f b
   -> f b
-(*>) =
-  error "todo: Course.Apply#(*>)"
+a *> b = const id <$> a <*> b
 
 -- | Sequence, discarding the value of the second argument.
 -- Pronounced, left apply.
@@ -234,8 +230,7 @@ lift4 =
   f b
   -> f a
   -> f b
-(<*) =
-  error "todo: Course.Apply#(<*)"
+b <* a = const <$> b <*> a
 
 -----------------------
 -- SUPPORT LIBRARIES --
